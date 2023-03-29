4c8e840a8a6b909e1ed38c02ffbb5e63da6b0a68
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-03-29 10:04:33 -0400

    Homework3

---
+++ src/Homework/Homework3.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        String firstString = "Hello world";
+        String secondString = "My name is Alex";
+        String thirdString = "What is your name?";
+        System.out.println(firstString);
+        System.out.println(secondString);
+        System.out.println(thirdString);
+    }
+}
