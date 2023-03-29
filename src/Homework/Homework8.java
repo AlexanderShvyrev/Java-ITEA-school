1c7c72d38b6880412b362ced3145e819c14153e3
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-03-29 13:12:24 -0400

    added homework 9

--- src/Homework/Homework8.java
+++ src/Homework/Homework8.java
@@ -2,10 +2,10 @@ package Homework;
 
 public class Homework8 {
     public static void main(String[] args) {
-        String uberflu? = "First string";
+        //String uberflu? = "First string";
         String _Identifier = "Second String";
-        String \Identifier = "Third string"; // illegal character
-        String &myVar = "Fourth string"; // cannot resolve symbol myVar
+        //String \Identifier = "Third string"; // illegal character
+        //String &myVar = "Fourth string"; // cannot resolve symbol myVar
         String myVariab1le = "Fifth string";
     }
 }
