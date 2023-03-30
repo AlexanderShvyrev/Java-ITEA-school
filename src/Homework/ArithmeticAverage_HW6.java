7dd33aa9cce18aa0ec8ad72359436612748ee623
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-03-30 08:52:43 -0400

    using double instead of int

--- src/Homework/ArithmeticAverage_HW6.java
+++ src/Homework/ArithmeticAverage_HW6.java
@@ -6,9 +6,9 @@ public class ArithmeticAverage_HW6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input three numbers for arithmetic average");
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int z = sc.nextInt();
+        double x = sc.nextInt();
+        double y = sc.nextInt();
+        double z = sc.nextInt();
         double ArAv = (x + y + z) / 3;
         System.out.println("Arithmetic average of your numbers is " + ArAv);
     }
