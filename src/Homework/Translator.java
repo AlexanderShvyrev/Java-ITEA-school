8b4081424ec14d440905202ec1c910d9a794ba41
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-04-10 13:31:23 -0400

    wip

--- src/Homework/Translator.java
+++ src/Homework/Translator.java
@@ -6,7 +6,7 @@ public class Translator {
     public static void main(String[] args) {
         String [] english_words = {"rain", "sunny", "cloudy", "snow", "wind", "warm", "cold", "humid", "thunderstorm", "overcast"};
         String [] russian_words = {"дождь", "солнечно", "облачно", "снег", "ветер", "тепло", "холодно", "влажно", "гроза", "пасмурно"};
-        System.out.println("Enter your english word and if I know it, I'll give you it's russian translation");
+        System.out.println("Enter your russian word and if I know it, I'll give you it's english translation");
         Scanner sc = new Scanner(System.in);
         String russianWord = sc.nextLine();
         String translated = "Нет такого слова";
