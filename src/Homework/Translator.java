bd99b1b4b3d320422ce7459eddcbcfde8adc31fa
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-04-10 13:29:53 -0400

    changed to use russian word input

--- src/Homework/Translator.java
+++ src/Homework/Translator.java
@@ -8,11 +8,11 @@ public class Translator {
         String [] russian_words = {"дождь", "солнечно", "облачно", "снег", "ветер", "тепло", "холодно", "влажно", "гроза", "пасмурно"};
         System.out.println("Enter your english word and if I know it, I'll give you it's russian translation");
         Scanner sc = new Scanner(System.in);
-        String englishWord = sc.nextLine();
+        String russianWord = sc.nextLine();
         String translated = "Нет такого слова";
-        for (int i = 0; i < english_words.length; i++) {
-            if(english_words[i].equals(englishWord)) { //Тут я використовую метод equals(), щоб порівняти 2 рядки. == порівняння не працює з рядками
-                translated = russian_words[i];
+        for (int i = 0; i < russian_words.length; i++) {
+            if(russian_words[i].equals(russianWord)) { //Тут я використовую метод equals(), щоб порівняти 2 рядки. == порівняння не працює з рядками
+                translated = english_words[i];
                 break;
             }
         }
