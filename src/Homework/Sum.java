162310c9a9115832c266f3317496323cff68e7da
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-04-19 15:23:24 -0400

    Sum of a and b

---
+++ src/Homework/Sum.java
@@ -0,0 +1,13 @@
+package Homework;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 10;
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of a and b is " + sum);
+    }
+}
