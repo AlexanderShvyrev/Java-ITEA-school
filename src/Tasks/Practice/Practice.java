616e70a3999afd221e1aa52ffdbe4bea95107f09
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-04-19 15:09:11 -0400

    Printing class

---
+++ src/Tasks/Practice/Practice.java
@@ -0,0 +1,63 @@
+package Tasks.Practice;
+
+public class Practice {
+    public static void main(String[] args) {
+//        int k = 2;
+//        int n = 10;
+//        for (int i = 0; i < n; i++) {
+//            System.out.println(k);
+//        }
+
+//        int a = 2;
+//        int b = 5;
+//        for (int i = a; i <= b ; i++) {
+//            System.out.println(i);
+//        }
+
+//        int a = 2;
+//        int b = 10;
+//        int count = 0;
+//        for (int i = b -1; i > a ; i--) {
+//            System.out.println(i);
+//            count++;
+//        }
+//        System.out.println("count is " + count);
+
+
+//        int a = 5;
+//        int b = 6;
+//        int result = a++ - -b + ++a + b++ + b - a - --a;
+//        System.out.println(result);
+
+        //For2. Даны два целых числа A и B (A < B). Вывести в порядке возрастания все
+        //целые числа, расположенные между A и B (включая сами числа A и B), а
+        //также количество N этих чисел.
+//        int a = 3;
+//        int b = 7;
+//        int count = 0;
+//        for (int i = a; i <=b ; i++) {
+//            System.out.println(i);
+//            count++;
+//        }
+//        System.out.println("count is " + count);
+
+//        For3. Даны два целых числа A и B (A < B). Вывести в порядке убывания все
+//        целые числа, расположенные между A и B (не включая числа A и B), а
+//        также количество N этих чисел.
+//            int a = 3;
+//            int b = 10;
+//            int n = 0;
+//        for (int i = b -1; i > a ; i--) {
+//            System.out.println(i);
+//            n++;
+//        }
+//        System.out.println("n is " + n);
+
+//        int n = 12245;
+//        int x = 0;
+//        for (int i = 10; n > 0 ; n/=i) {
+//            x = n%i;
+//            System.out.println(x);
+//        }
+    }
+}
