3f3285c462f49cccd877935306c9f7a331a0ef10
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-05-05 12:55:50 -0400

    fix

--- src/Homework/PowerA3.java
+++ src/Homework/PowerA3.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class PowerA3 {
-    public static int powerA3(int a) {
-        return a * a * a;
+    public static int powerA3(int a, int b) {
+        b = a * a * a;
+        return b;
     }
 
     public static void main(String[] args) {
-        int[] b = new int[]{powerA3(2), powerA3(10), powerA3(20), powerA3(3), powerA3(4)};
-        System.out.println(Arrays.toString(b));
+        int[] result = new int[]{powerA3(2, 0), powerA3(10, 0), powerA3(20, 0), powerA3(3, 0), powerA3(4, 0)};
+        System.out.println(Arrays.toString(result));
     }
 }
