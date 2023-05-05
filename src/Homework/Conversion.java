bf0eed402997940b82fda4a04a66acf4de485bf0
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-05-05 11:41:40 -0400

    conversion

---
+++ src/Homework/Conversion.java
@@ -0,0 +1,17 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Conversion {
+    public static double convert(double amount, double rate){
+        return amount * rate;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter an amount: ");
+        double amount = sc.nextDouble();
+        System.out.println("Enter the conversion rate: ");
+        double rate = sc.nextDouble();
+        System.out.println(convert(amount, rate));
+    }
+}
