40dbb0de07445572cf687e5d82f6a0944a8df668
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-05-05 13:48:15 -0400

    fixed bug

--- src/Homework/CalculatorV2.java
+++ src/Homework/CalculatorV2.java
@@ -2,10 +2,10 @@ package Homework;
 
 public class CalculatorV2 {
 
-    public static double midArithmetic(int a, int b, int c){
-        return (a + b + c ) / 3;
+    public static float midArithmetic(int a, int b, int c){
+        return (a + b + c ) / 3f;
     }
     public static void main(String[] args) {
-        System.out.println(midArithmetic(2,3,4));
+        System.out.println(midArithmetic(2,4,4));
     }
 }
