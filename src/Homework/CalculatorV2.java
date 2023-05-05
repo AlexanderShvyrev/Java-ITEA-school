0308812b040ec7dd61c3f6954f7def1f5ff4baa9
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-05-05 13:19:23 -0400

    wip

---
+++ src/Homework/CalculatorV2.java
@@ -0,0 +1,11 @@
+package Homework;
+
+public class CalculatorV2 {
+
+    public static double midArithmetic(int a, int b, int c){
+        return (a + b + c ) / 3;
+    }
+    public static void main(String[] args) {
+        System.out.println(midArithmetic(2,3,4));
+    }
+}
