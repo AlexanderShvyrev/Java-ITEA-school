65e75b09b9e3b7424d2c63abc66ba433b0d79858
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-05-05 14:36:06 -0400

    wip

---
+++ src/Homework/DeliveryV2.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class DeliveryV2 {
+
+    public static int calculateRoutes(int customers){
+        if(customers == 0){
+            return 1;
+        }
+        return customers * calculateRoutes(customers - 1);
+    }
+    public static void main(String[] args) {
+        int customers = 10;
+        int routes = calculateRoutes((customers));
+        System.out.println("You could have " + routes);
+    }
+}
