ce28943c85db2c93ba936d0e328a78e3485890f5
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-05-31 11:02:17 -0400

    wip

--- src/Homework/OOP/Player/Main.java
+++ src/Homework/OOP/Player/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Player player = new Player();
 
-        ((Playable) player).play();
-        ((Playable) player).pause();
-        ((Playable) player).stop();
+        player.play();
+        player.pause();
+        player.stop();
 
-        ((Recordable) player).record();
-        ((Recordable) player).pause();
-        ((Recordable) player).stop();
+        player.record();
+        player.pause();
+        player.stop();
     }
 }
