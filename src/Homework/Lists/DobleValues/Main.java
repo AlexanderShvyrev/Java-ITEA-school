9390458e9f5b11178e298ece72bc17994b2f5ebc
Alexander Shvyrev <shurik_stv@yahoo.com>
2023-07-10 10:39:47 -0400

    double words

---
+++ src/Homework/Lists/DobleValues/Main.java
@@ -0,0 +1,35 @@
+package Homework.Lists.DobleValues;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter 5 words: ");
+
+        for (int i = 0; i < 5; i++) {
+            String word = sc.nextLine();
+            words.add(word);
+        }
+
+        doubleWords(words);
+
+        System.out.println("Output: ");
+
+        for (String word:words) {
+            System.out.println(word);
+        }
+    }
+
+    public static void doubleWords(List<String> words){
+        for (int i = 0; i < words.size(); i++) {
+            String letter = words.get(i);
+            words.add(i + 1, letter);
+            i++;
+        }
+    }
+}
